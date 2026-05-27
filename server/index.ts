44e3192514fca5c8fe8a0eb379acde0009efe354
Hevini Sporting <contact@hevini.com>
2026-05-27 19:01:03 -0400

    Add Hevini logo assets

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
@@ -106,8 +107,8 @@ app.use((req, res, next) => {
   httpServer.listen(
     {
       port,
-      host: "0.0.0.0",
-      reusePort: true,
+      host: "localhost",
+      reusePort: false,
     },
     () => {
       log(`serving on port ${port}`);
